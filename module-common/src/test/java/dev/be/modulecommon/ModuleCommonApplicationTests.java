0c26a88230c2c7826385bec64dc4cb1ce447af9f
goodGid <hello.goodgid@gmail.com>
2022-07-10 23:28:20 +0900

    common module 생성

---
+++ module-common/src/test/java/dev/be/modulecommon/ModuleCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.be.modulecommon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModuleCommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
