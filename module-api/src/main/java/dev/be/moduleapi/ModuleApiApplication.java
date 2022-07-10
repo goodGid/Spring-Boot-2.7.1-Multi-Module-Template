5cc048b0386f7af79c6d717fe2a717ba30f6c9e7
goodGid <hello.goodgid@gmail.com>
2022-07-10 23:27:47 +0900

    api module 생성

---
+++ module-api/src/main/java/dev/be/moduleapi/ModuleApiApplication.java
@@ -0,0 +1,13 @@
+package dev.be.moduleapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuleApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModuleApiApplication.class, args);
+    }
+
+}
