0b92e5c7e9cc9784e5abac0aac14f47357007f81
goodGid <hello.goodgid@gmail.com>
2022-07-10 23:25:45 +0900

    root project 생성

---
+++ src/main/java/dev/be/multimodule/MultimoduleApplication.java
@@ -0,0 +1,13 @@
+package dev.be.multimodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultimoduleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultimoduleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/be/multimodule/MultimoduleApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.be.multimodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultimoduleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
