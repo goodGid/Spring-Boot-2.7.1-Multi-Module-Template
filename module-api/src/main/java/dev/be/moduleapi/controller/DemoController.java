78fbdf32280ef8ff98778d3500e740e25a8e6bd9
goodGid <hello.goodgid@gmail.com>
2022-07-14 07:42:31 +0900

    api module에 controller, service package 생성

---
+++ module-api/src/main/java/dev/be/moduleapi/controller/DemoController.java
@@ -0,0 +1,24 @@
+package dev.be.moduleapi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import dev.be.moduleapi.service.DemoService;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+public class DemoController {
+
+    private final DemoService demoService;
+
+    @GetMapping("/save")
+    public String save() {
+        return demoService.save();
+    }
+
+    @GetMapping("/find")
+    public String find() {
+        return demoService.find();
+    }
+}
